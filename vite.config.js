13f47d1e6b5b737830b572f64b32ea283c9d9711
Ueslei Paim <uesleibros@gmail.com>
2026-02-02 03:38:07 -0300

    Remove vite-plugin-mix from plugins
    Removed vite-plugin-mix from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import mix from 'vite-plugin-mix'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
-    mix({
-      handler: "./api/index.js"
-    }),
   ],
 })
