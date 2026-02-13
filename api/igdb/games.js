300c3b70e2379abd9676777976be68d7979fa255
Ueslei Paim <uesleibros@gmail.com>
2026-02-13 14:04:58 -0300

    Update games.js

--- api/igdb/games.js
+++ api/igdb/games.js
@@ -59,7 +59,7 @@ export default async function handler(req, res) {
              game_type;
       where ${nameFilter}
         & game_type = (0,2,4,8,9,10)
-        & cover != null
+        & cover != null;
       sort total_rating_count desc;
       limit 30;
     `
