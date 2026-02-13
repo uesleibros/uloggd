134d69af9a7fffd74197eb87a03429d74f5cdb7e
Ueslei Paim <uesleibros@gmail.com>
2026-02-13 03:18:13 -0300

    Add category filter to IGDB game search query

--- api/igdb/games.js
+++ api/igdb/games.js
@@ -38,6 +38,7 @@ export default async function handler(req, res) {
       },
       body: `
         search "${safeQuery}";
+        where category = (0,8,9);
         fields 
           id,
           name,
