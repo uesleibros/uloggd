06885c70c30a5b817e6f48d6fa5df33478ba69e8
Ueslei Paim <uesleibros@gmail.com>
2026-02-17 00:04:01 -0300

    Update Header.jsx

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -473,7 +473,7 @@ export default function Header() {
 
     timeoutRef.current = setTimeout(async () => {
       try {
-        const res = await fetch("/api/igdb/autocomplete", {
+        const res = await fetch("/api/igdb?action=autocomplete", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ query }),
