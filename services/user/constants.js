33840e7acc129b1eff7a2693c08352750d2286d6
Ueslei Paim <uesleibros@gmail.com>
2026-02-18 03:08:16 -0300

    Update constants.js

--- services/user/constants.js
+++ services/user/constants.js
@@ -3,22 +3,3 @@ export const MAX_BANNER_SIZE = 5 * 1024 * 1024
 export const VALID_FOLLOW_ACTIONS = ["follow", "unfollow"]
 export const VALID_LIST_TYPES = ["followers", "following"]
 export const VALID_BANNER_ACTIONS = ["upload", "remove"]
-export const VALID_BORDERS = [
-  "default",
-  "premium",
-  "mod",
-  "gold",
-  "plasma",
-  "glitch",
-  "neon",
-  "fire",
-  "ice",
-  "matrix",
-  "galaxy",
-  "lightning",
-  "void",
-  "heartbeat",
-  "toxic",
-  "sakura",
-  "rgb"
-]
