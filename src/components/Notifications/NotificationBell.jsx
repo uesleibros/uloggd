30c90c477fae7814b33475f2cea6a12698072bf5
Ueslei Paim <uesleibros@gmail.com>
2026-02-20 01:33:03 -0300

    Update NotificationBell.jsx

--- src/components/Notifications/NotificationBell.jsx
+++ src/components/Notifications/NotificationBell.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { Bell } from "lucide-react"
-import { useNotifications } from "../../hooks/useNotifications"
+import { useNotifications } from "../../..//hooks/useNotifications"
 import NotificationPanel from "./NotificationPanel"
 
 export default function NotificationBell() {
