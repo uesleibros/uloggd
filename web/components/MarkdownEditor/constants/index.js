28d4229c5e4357fc043ac2a1024a8079f7c2f8b3
Ueslei Paim <uesleibros@gmail.com>
2026-02-22 15:36:19 -0300

    Update index.js

--- web/components/MarkdownEditor/constants/index.js
+++ web/components/MarkdownEditor/constants/index.js
@@ -2,3 +2,5 @@ export * from "./toolbar"
 export * from "./features"
 export * from "./alertVariants"
 export * from "./schema"
+
+export const NO_COVER_IMAGE = "https://images.igdb.com/igdb/image/upload/t_cover_big/nocover.png"
