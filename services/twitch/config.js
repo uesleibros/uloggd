d2298331a4bfe42ce19cf47fb36c4b76f882fc5e
Ueslei Paim <uesleibros@gmail.com>
2026-02-23 02:56:01 -0300

    Create config.js

---
+++ services/twitch/config.js
@@ -0,0 +1,6 @@
+export const TWITCH_CONFIG = {
+	authUrl: "https://id.twitch.tv/oauth2/authorize",
+	tokenUrl: "https://id.twitch.tv/oauth2/token",
+	apiUrl: "https://api.twitch.tv/helix",
+	scopes: ["user:read:email"]
+}
