42470e94a1c7e3f5dd3b3688e8150d3371c0b39c
Ueslei Paim <uesleibros@gmail.com>
2026-02-23 23:11:26 -0300

    Update steam.js

--- routers/steam.js
+++ routers/steam.js
@@ -2,12 +2,14 @@ import { handleLogin } from "#services/steam/handlers/login.js"
 import { handleCallback } from "#services/steam/handlers/callback.js"
 import { handleStatus } from "#services/steam/handlers/status.js"
 import { handleDisconnect } from "#services/steam/handlers/disconnect.js"
+import { handlePresence } from "#services/steam/handlers/presence.js"
 import { getUser } from "#lib/auth.js"
 
 const ACTIONS = {
 	login:      { handler: handleLogin,       method: "POST", auth: true  },
 	callback:   { handler: handleCallback,    method: "GET", 	auth: false },
 	status:     { handler: handleStatus,      method: "POST", auth: false },
+	presence:   { handler: handlePresence,    method: "POST", auth: false },
 	disconnect: { handler: handleDisconnect,	method: "POST", auth: true	}
 }
 
