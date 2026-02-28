2d080ce01674fee46dbb10f2303100caf8f54e83
Ueslei Paim <uesleibros@gmail.com>
2026-02-28 00:18:46 -0300

    Update AvatarSection.jsx

--- web/components/User/Settings/sections/AvatarSection.jsx
+++ web/components/User/Settings/sections/AvatarSection.jsx
@@ -1,9 +1,15 @@
+import { useTranslation } from "#hooks/useTranslation"
 import SettingsSection from "@components/User/Settings/ui/SettingsSection"
 import AvatarEditor from "@components/User/AvatarEditor"
 
 export default function AvatarSection({ avatar, onSave, saving }) {
+  const { t } = useTranslation()
+
   return (
-    <SettingsSection title="Avatar" description="Personalize o avatar do seu perfil. Recomendado: 512x512px.">
+    <SettingsSection
+      title={t("settings.avatarSection.title")}
+      description={t("settings.avatarSection.description")}
+    >
       <AvatarEditor currentAvatar={avatar} onSave={onSave} saving={saving} />
     </SettingsSection>
   )
