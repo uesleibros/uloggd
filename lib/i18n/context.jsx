25cdf2ca27262b0184436e04c6ded3d70790383e
UesleiDev <63914812+uesleibros@users.noreply.github.com>
2026-02-27 21:41:42 -0300

    melhorado tier

--- lib/i18n/context.jsx
+++ lib/i18n/context.jsx
@@ -18,8 +18,8 @@ import {
   getLanguageDirection,
 } from "./utils"
 
-import pt from "@/locales/pt.json"
-import en from "@/locales/en.json"
+import pt from "@/locales/pt"
+import en from "@/locales/en"
 
 const translations = { pt, en }
 
