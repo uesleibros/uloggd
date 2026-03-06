24a8082151eab94e481a401ad8c35edaf1d87712
Ueslei Paim <uesleibros@gmail.com>
2026-03-06 01:24:27 -0300

    Update ListsSection.jsx

--- web/components/Lists/ListsSection.jsx
+++ web/components/Lists/ListsSection.jsx
@@ -214,7 +214,7 @@ export default function ListsSection({
         </div>
       ) : (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-3">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
             {lists.map(list => (
               <ListCard
                 key={list.id}
@@ -259,5 +259,4 @@ export default function ListsSection({
       />
     </div>
   )
-
 }
