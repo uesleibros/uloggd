2e64a0f214357a9084491f8678d736090d501058
Ueslei Paim <uesleibros@gmail.com>
2026-03-08 03:38:14 -0300

    Update ProfileSection.jsx

--- web/components/User/Settings/sections/ProfileSection.jsx
+++ web/components/User/Settings/sections/ProfileSection.jsx
@@ -56,7 +56,7 @@ export default function ProfileSection({ user, onUsernameSave, usernameSaving })
         <AvatarWithDecoration
           src={user.avatar}
           alt={user.username}
-          decoration={user.avatar_decoration}
+          decorationUrl={user.equipped?.avatar_decoration?.asset_url}
           status={getStatus(user.last_seen, user.status)}
           isStreaming={!!user.stream}
           size="xl"
@@ -188,3 +188,4 @@ export default function ProfileSection({ user, onUsernameSave, usernameSaving })
     </SettingsSection>
   )
 }
+
