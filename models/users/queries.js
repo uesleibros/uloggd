434c711ea556e798781a5f9ee48a6ecb02942901
Ueslei Paim <uesleibros@gmail.com>
2026-03-08 01:41:54 -0300

    Update queries.js

--- models/users/queries.js
+++ models/users/queries.js
@@ -5,20 +5,7 @@ const MINERALS_REL = `user_minerals ( copper, iron, gold, emerald, diamond, ruby
 const CONNECTIONS_FULL = `user_connections ( provider, provider_user_id, provider_username, provider_display_name )`
 const CONNECTIONS_TWITCH = `user_connections ( provider, provider_username )`
 
-const EQUIPPED_REL = `
-  user_equipped_items (
-    slot,
-    inventory:user_inventory (
-      item:store_items (
-        id,
-        slug,
-        name,
-        asset_url,
-        item_type
-      )
-    )
-  )
-`
+const EQUIPPED_REL = `user_equipped_items ( slot, inventory:user_inventory ( item:store_items ( id, slug, name, asset_url, item_type ) ) )`
 
 const FULL_FIELDS = `
   user_id, username, banner, bio, pronoun, thinking, avatar,
@@ -190,6 +177,7 @@ export async function getFollowStatus(userId, currentUserId) {
     followsYou: !!followsYouRes.data,
   }
 }
+
 
 
 
