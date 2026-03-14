748b681c1f3e81cfbc8dfbc3934e616da9746d82
Ueslei Paim <uesleibros@gmail.com>
2026-03-14 15:50:06 -0300

    Update MarkdownPreview.jsx

--- web/components/MarkdownEditor/MarkdownPreview.jsx
+++ web/components/MarkdownEditor/MarkdownPreview.jsx
@@ -45,6 +45,8 @@ export const MarkdownPreview = memo(function MarkdownPreview({
     )
   }
 
+  console.log("MarkdownPreview ownerId:", ownerId)
+
   return (
     <GamesBatchProvider ownerId={ownerId}>
       <div className="markdown-body">
