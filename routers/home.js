75a1459e31b364e64770d247c101dc62cbe1e155
Ueslei Paim <uesleibros@gmail.com>
2026-03-19 02:35:25 -0300

    Update home.js

--- routers/home.js
+++ routers/home.js
@@ -1,12 +1,14 @@
 import { handlePopularAmongFriends } from "#services/home/handlers/popularAmongFriends.js"
 import { handleFriendsReviews } from "#services/home/handlers/friendsReviews.js"
 import { handlePopularLists } from "#services/home/handlers/popularLists.js"
+import { handlePopularScreenshots } from "#services/home/handlers/popularScreenshots.js"
 import { getUser } from "#lib/auth.js"
 
 const ACTIONS = {
   popularAmongFriends: { handler: handlePopularAmongFriends, method: "GET", auth: true },
   friendsReviews: { handler: handleFriendsReviews, method: "GET", auth: true },
   popularLists: { handler: handlePopularLists, method: "GET", auth: false },
+  popularScreenshots: { handler: handlePopularScreenshots, method: "GET", auth: false },
 }
 
 export async function homeHandler(req, res) {
